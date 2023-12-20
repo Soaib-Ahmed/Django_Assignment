1f6e1780b25b9840654259770fd0a40ae0eb58ca
Soaib Ahmed <113377540+Soaib-Ahmed@users.noreply.github.com>
2023-12-20 14:49:09 +0600

    Update forms.py

--- Car_Sales_Project/cars/forms.py
+++ Car_Sales_Project/cars/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Car
+from .models import Car,CarComment
 
 class CarForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = '__all__'
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = CarComment
+        fields = ['name', 'body']
